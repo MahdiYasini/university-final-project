7c70a856f78d34c90125b42b3b6bfbf714600281
Mahdi Yasini <yasinimahdi98@gmail.com>
2020-12-09 22:07:48 +0330

    User schema

---
+++ Models/User.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+const UserSchema = new mongoose.Schema({
+    userName: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    profileImage: {
+        type: String,
+        default: '/images/profileImages/defaultIamge'
+    },
+    description: {
+        type: String,
+        default: "یه ماجرا جو"
+    },
+    timestamps: true
+});
+module.exports = mongoose.model('User', UserSchema);

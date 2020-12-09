991d6058eb838856a1e8f949ed9294f6c3af1e9d
Mahdi Yasini <yasinimahdi98@gmail.com>
2020-12-09 23:29:30 +0330

    NI

--- app.js
+++ app.js
@@ -11,11 +11,13 @@ var app = express();
 
 //********************* <<setup to access public folder>> *********************//
 app.use(express.static(path.join(__dirname, 'public')));
+//********************* //
 
 //********************* <<Setup body parser middleware>> *********************//
 const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: false }));
 //app.use(bodyParser.json()) 
+//********************* //
 
 //********************* <<Setup Database>> *********************//
 const mongoose = require('mongoose');
@@ -32,26 +34,27 @@ mongoose.connect(DbUrl, {
     });
 //********************* //
 
-
-
-
-// view engine setup
+//********************* <<view engine setup>> *********************//
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+//********************* //
 
+//********************* <<More setup>> *********************//
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
+//********************* <<Routes>> *********************//
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+//********************* //
 
-// catch 404 and forward to error handler
+//********************* <<catch 404 and forward to error handler>> *********************//
 app.use(function(req, res, next) {
   next(createError(404));
 });
+//********************* //
 
 // error handler
 app.use(function(err, req, res, next) {

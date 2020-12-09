ce293cb27a2b9c65f9cb30dcf798ddd6bdafc33b
Mahdi Yasini <yasinimahdi98@gmail.com>
2020-12-09 22:33:40 +0330

    Post Schema

---
+++ Models/Post.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+
+const PostSchema = new mongoose.Schema({
+    subject: {
+        type: String,
+        required: true
+    },
+    article: {
+        type: String,
+        required: true
+    },
+    summery: {
+        type: String,
+        required: true,
+    },
+    image: {
+        type: String,
+        required: true,
+        default: '/images/postImages/defaultIamge'
+    },
+    author: {
+        type: Schema.Types.ObjectId, 
+        ref: 'User'
+    },
+    timestamps: true
+});
+module.exports  = mongoose.model('Post', PostSchema);

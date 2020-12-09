d06e588c414a620d92ae08135571b3546ba3131c
Mahdi Yasini <yasinimahdi98@gmail.com>
2020-12-09 23:33:03 +0330

    NI

--- app.js
+++ app.js
@@ -44,6 +44,7 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+//********************* //
 
 //********************* <<Routes>> *********************//
 app.use('/', indexRouter);
@@ -56,15 +57,15 @@ app.use(function(req, res, next) {
 });
 //********************* //
 
-// error handler
+//********************* <<Error Handler>> *********************//
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
-
   // render the error page
   res.status(err.status || 500);
   res.render('error');
 });
+//********************* //
 
 module.exports = app;

ee294011366c3ff97bc3e5e7ae13832485579959
Mahdi Yasini <yasinimahdi98@gmail.com>
2020-12-11 20:48:52 +0330

    Register request handle and improve code

--- routes/users.js
+++ routes/users.js
@@ -2,11 +2,16 @@
 var express = require('express');
 var router = express.Router();
 
+const path = require("path");
+
+const bcrypt = require("bcryptjs");
+
 const User = require('../Models/User');
 const Post = require('../Models/Post');
 
 //********************* <<Setup Multer for save file in local storage>> *********************//
 const multer = require("multer");
+const { exit } = require('process');
 // For save profile image.
 let uploadProfileImage = multer({
   storage: multer.diskStorage({
@@ -21,13 +26,21 @@ let uploadProfileImage = multer({
     }
   }),
   fileFilter: function (req, file, callback) {
-    if (path.extname(file.originalname) !== ".png" && ".jpeg" && ".jpg" && ".gif") {
+    console.log('file :>> ', file);
+    //! The below code didn't work, I don't know why.
+    // if (path.extname(file.originalname) !== ".png" && ".jpeg" && ".jpg" && ".gif")
+    if (
+      path.extname(file.originalname) !== ".png" &&
+      path.extname(file.originalname) !== ".gif" &&
+      path.extname(file.originalname) !== ".jpg" &&
+      path.extname(file.originalname) !== ".jpeg"
+    ) {
       return callback(null, flase);
     }
     callback(null, true);
   },
   limits: {
-    fileSize: 2000000
+    fileSize: 7000000
   }
 });
 
@@ -46,19 +59,19 @@ let uploadArticleImage = multer({
   }),
   fileFilter: function (req, file, callback) {
     if (path.extname(file.originalname) !== ".png" && ".gif" && ".jpg" && ".jpeg") {
-      return callback(null, flase);
+      return callback(null, false);
     }
     callback(null, true);
   },
   limits: {
-    fileSize: 2000000
+    fileSize: 7000000
   }
 });
 //********************* //
 
 
-//********************* <<Hanlde Registering request >> *********************//
-// Register Page request
+//********************* <<Handle Registering request >> *********************//
+//**** Register Page request
 router.get("/register", (req, res) => {
   if (req.user) {
     //When user logged in.
@@ -68,11 +81,128 @@ router.get("/register", (req, res) => {
   }
 });
 
-//Register request
+//**** Register request handle
+router.post("/register", uploadProfileImage.single("profilePicture"), (req, res) => {
+  const {
+    userName,
+    email,
+    password,
+    password2,
+    description
+  } = req.body;
 
+  //Define for add probable errors.
+  let errors = [];
+
+  // Check required fields
+  if (!userName || !email || !password || !password2) {
+    errors.push({
+      msg: "لطفااطلاعات را کامل کنید"
+    });
+  };
+
+  // Check matching passwords
+  if (password !== password2) {
+    errors.push({
+      msg: "تکرار رمز صحیح نیست"
+    });
+  };
+
+  //Check password length
+  if (password.length < 6) {
+    errors.push({
+      msg: "حداقل 6 حرف (کارکتر) برای رمز"
+    });
+  }
+
+  //Check any error exist or not.
+  if (errors.length > 0) {
+    res.render("register", {
+      errors,
+      userName,
+      email,
+      password,
+      password2
+    });
+  }
+  else {
+    //! We have to use else because solve the error of the below  
+    // Cannot set headers after they are sent to the client
+
+    // Validation for existence user 
+    User.findOne({
+      userName: userName
+    }).then(user => {
+      if (user) {
+        errors.push({
+          msg: "نام کاربری قبلا ثبت نام کرده است"
+        });
+        res.render("register", {
+          errors,
+          userName,
+          email,
+          password,
+          password2
+        });
+      }
+    });
+
+    // Validation for existence email 
+    User.findOne({
+      email: email
+    }).then(user => {
+      if (user) {
+        errors.push({
+          msg: "آدرس رایانامه قبلا ثبت شده است"
+        });
+        res.render("register", {
+          errors,
+          userName,
+          email,
+          password,
+          password2
+        });
+      }
+    });
+
+    // Create new user
+    const newUser = new User({
+      userName,
+      email,
+      password,
+      description
+    });
+
+    // If user upload profile picture
+    if (req.file) {
+      newUser.image = "/uploads/profile/" + req.file.filename;
+    }
+    if (description) {
+      newUser.description = description;
+    }
+
+    // Hashing password
+    bcrypt.genSalt(10, (err, salt) =>
+      bcrypt.hash(newUser.password, salt, (err, hash) => {
+        if (err) throw err;
+        newUser.password = hash;
+        //Save user to DB.
+        newUser
+          .save()
+          .then(user => {
+            req.flash("success_msg", "شما ثبت نام کردید و میتوانید وارد شوید");
+            res.redirect("/login");
+          })
+          .catch(err => console.log(err));
+      })
+    );
+  }
+});
 //********************* //
+
+
 /* GET users listing. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   req.flash("success_msg", "شما ثبت نام کردید و میتوانید وارد شوید");
   res.redirect("/");
 

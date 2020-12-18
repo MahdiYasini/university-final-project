159f6f5f7b88b56e56a312ce50c8818ddb7ee2cf
Mahdi Yasini <yasinimahdi98@gmail.com>
2020-12-19 00:32:50 +0330

    NI

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var express = require('express');
 var router = express.Router();
 
 const Post = require('../Models/Post');
-const User = require ('../Models/User');
+const User = require('../Models/User');
 
 const path = require("path");
 const bcrypt = require("bcryptjs");
@@ -48,132 +48,132 @@ let uploadProfileImage = multer({
 //********************* <<Handle Registering request >> *********************//
 //**** Register Page request
 router.get("/register", (req, res) => {
-    console.log('req.user :>> ', req.user);
-    if (req.user) {
-      res.redirect("dashboard");
-    } else {
-      res.render("register");
-    }
-  });
-  
-  //**** Register request handle
-  router.post("/register", uploadProfileImage.single("profilePicture"), (req, res, next) => {
-    const {
+  console.log('req.user :>> ', req.user);
+  if (req.user) {
+    res.redirect("dashboard");
+  } else {
+    res.render("register");
+  }
+});
+
+//**** Register request handle
+router.post("/register", uploadProfileImage.single("profilePicture"), (req, res, next) => {
+  const {
+    userName,
+    email,
+    password,
+    password2,
+    description
+  } = req.body;
+
+  //Define for add probable errors.
+  let errors = [];
+
+  // Check required fields
+  if (!userName || !email || !password || !password2) {
+    errors.push({
+      msg: "لطفااطلاعات را کامل کنید"
+    });
+  };
+
+  // Check matching passwords
+  if (password !== password2) {
+    errors.push({
+      msg: "تکرار رمز صحیح نیست"
+    });
+  };
+
+  //Check password length
+  if (password.length < 6) {
+    errors.push({
+      msg: "حداقل 6 حرف (کارکتر) برای رمز"
+    });
+  }
+
+  //Check any error exist or not.
+  if (errors.length > 0) {
+    res.render("register", {
+      errors,
       userName,
       email,
       password,
-      password2,
+      password2
+    });
+  }
+  else {
+    //! We have to use else because solve the error of the below  
+    // Cannot set headers after they are sent to the client
+
+    // Validation for existence user 
+    User.findOne({
+      userName: userName
+    }).then(user => {
+      if (user) {
+        errors.push({
+          msg: "نام کاربری قبلا ثبت نام کرده است"
+        });
+        res.render("register", {
+          errors,
+          userName,
+          email,
+          password,
+          password2
+        });
+      }
+    });
+
+    // Validation for existence email 
+    User.findOne({
+      email: email
+    }).then(user => {
+      if (user) {
+        errors.push({
+          msg: "آدرس رایانامه قبلا ثبت شده است"
+        });
+        res.render("register", {
+          errors,
+          userName,
+          email,
+          password,
+          password2
+        });
+      }
+    });
+
+    // Create new user
+    const newUser = new User({
+      userName,
+      email,
+      password,
       description
-    } = req.body;
-  
-    //Define for add probable errors.
-    let errors = [];
-  
-    // Check required fields
-    if (!userName || !email || !password || !password2) {
-      errors.push({
-        msg: "لطفااطلاعات را کامل کنید"
-      });
-    };
-  
-    // Check matching passwords
-    if (password !== password2) {
-      errors.push({
-        msg: "تکرار رمز صحیح نیست"
-      });
-    };
-  
-    //Check password length
-    if (password.length < 6) {
-      errors.push({
-        msg: "حداقل 6 حرف (کارکتر) برای رمز"
-      });
+    });
+
+    // If user upload profile picture
+    if (req.file) {
+      newUser.image = "/uploads/profile/" + req.file.filename;
     }
-  
-    //Check any error exist or not.
-    if (errors.length > 0) {
-      res.render("register", {
-        errors,
-        userName,
-        email,
-        password,
-        password2
-      });
+    if (description) {
+      newUser.description = description;
     }
-    else {
-      //! We have to use else because solve the error of the below  
-      // Cannot set headers after they are sent to the client
-  
-      // Validation for existence user 
-      User.findOne({
-        userName: userName
-      }).then(user => {
-        if (user) {
-          errors.push({
-            msg: "نام کاربری قبلا ثبت نام کرده است"
-          });
-          res.render("register", {
-            errors,
-            userName,
-            email,
-            password,
-            password2
-          });
-        }
-      });
-  
-      // Validation for existence email 
-      User.findOne({
-        email: email
-      }).then(user => {
-        if (user) {
-          errors.push({
-            msg: "آدرس رایانامه قبلا ثبت شده است"
-          });
-          res.render("register", {
-            errors,
-            userName,
-            email,
-            password,
-            password2
-          });
-        }
-      });
-  
-      // Create new user
-      const newUser = new User({
-        userName,
-        email,
-        password,
-        description
-      });
-  
-      // If user upload profile picture
-      if (req.file) {
-        newUser.image = "/uploads/profile/" + req.file.filename;
-      }
-      if (description) {
-        newUser.description = description;
-      }
-  
-      // Hashing password
-      bcrypt.genSalt(10, (err, salt) =>
-        bcrypt.hash(newUser.password, salt, (err, hash) => {
-          if (err) throw err;
-          newUser.password = hash;
-          //Save user to DB.
-          newUser
-            .save()
-            .then(user => {
-              req.flash("success_msg", "شما ثبت نام کردید و میتوانید وارد شوید");
-              res.redirect("/login");
-            })
-            .catch(err => console.log(err));
-        })
-      );
-    }
-  });
-  //********************* //
+
+    // Hashing password
+    bcrypt.genSalt(10, (err, salt) =>
+      bcrypt.hash(newUser.password, salt, (err, hash) => {
+        if (err) throw err;
+        newUser.password = hash;
+        //Save user to DB.
+        newUser
+          .save()
+          .then(user => {
+            req.flash("success_msg", "شما ثبت نام کردید و میتوانید وارد شوید");
+            res.redirect("/login");
+          })
+          .catch(err => console.log(err));
+      })
+    );
+  }
+});
+//********************* //
 
 
 
@@ -182,21 +182,21 @@ router.get("/register", (req, res) => {
 //********************* <<Handle Login request >> *********************//
 //**** Login Page request
 router.get("/login", (req, res) => {
-    if (req.user) {
-      res.redirect('dashboard')
-    }
-    else res.render("login");
-  });
-  
-  //**** Login request handle
-  router.post("/login", (req, res, next) => {
-    passport.authenticate("local", {
-      successRedirect: "/dashboard",
-      failureRedirect: "/login",
-      failureFlash: true
-    })(req, res, next);
-  });
-  //********************* //
+  if (req.user) {
+    res.redirect('dashboard')
+  }
+  else res.render("login");
+});
+
+//**** Login request handle
+router.post("/login", (req, res, next) => {
+  passport.authenticate("local", {
+    successRedirect: "/dashboard",
+    failureRedirect: "/login",
+    failureFlash: true
+  })(req, res, next);
+});
+//********************* //
 
 
 
@@ -211,7 +211,5 @@ router.get("/login", (req, res) => {
 //     })
 //   });
 // });
-
-
 
 module.exports = router;

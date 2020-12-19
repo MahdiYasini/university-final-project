d20eb024620c015795947d9812a1e6f093351f9a
Mahdi Yasini <yasinimahdi98@gmail.com>
2020-12-19 21:37:59 +0330

    NI

--- public/javascripts/editProfile.js
+++ public/javascripts/editProfile.js
@@ -1,10 +1,8 @@
-console.log("Check");
 $( document ).ready(function() {
     $("#changePasswordInput").hide();
     $("#country").hide();
     console.log( "ready!" );
     $("#changePassword").click(() => {
-        console.log("clicked");
         $('#changePasswordDiv').hide("slow");
         $("#changePasswordInput").show("slow");
         

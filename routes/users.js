081dfecd06305d8c3be9591897217898313f2490
Mahdi Yasini <yasinimahdi98@gmail.com>
2020-12-19 21:37:38 +0330

    myArticle handle (request, page and style)

--- routes/users.js
+++ routes/users.js
@@ -216,6 +216,49 @@ router.post('/editProfile', uploadProfileImage.single("profilePicture"), (req, r
     })
     .catch(err => console.log(err));
 });
+//********************* //
+
+//********************* <<Handle myArticles request >> *********************//
+router.get("/myArticles", (req, res) => {
+  Post.find({
+    author: req.user._id
+  }).then(posts => {
+    let checkExistPost = 0;
+    if(posts.length == 0) checkExistPost = 1;
+    
+    //! بعد از اینکه پست اضافه کردی حواست باشه برای نشون دادن زمان پست به صورت شمسی باید کد های زیر رو اعمال کنیم 
+    // for (var data in posts) {
+    //   //Set Hour
+    //   let setHour = posts[data].date.getHours() - 3;
+    //   if (setHour < 0) {
+    //     setHour += 24;
+    //   }
+
+    //   //Set Minute
+    //   let setMinute = posts[data].date.getMinutes() - 30;
+    //   if (setMinute < 0) {
+    //     setMinute += 60;
+    //     setHour -= 1;
+    //   }
+
+    //   //Set Second
+    //   let setSecond = posts[data].date.getSeconds();
+    //   if (setSecond < 0) {
+    //     setSecond += 60;
+    //     setMinute -= 1;
+    //   }
+
+    //   posts[data]["time"] = `${setHour}:${setMinute}:${setSecond}`;
+    //   posts[data]["dateCalender"] = moment(`${posts[data].date.getFullYear()}/${posts[data].date.getMonth()}/${posts[data].date.getDate()}`, "YYYY/MM/DD").locale("fa").format("YYYY/MM/DD");
+    // }
+
+    res.render("myArticles", {
+      blogPost: posts,
+      checkExistPost: checkExistPost
+    });
+  });
+});
+//********************* //
 
 /* GET users listing. */
 router.get('/', function (req, res, next) {

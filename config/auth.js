b481a71b0b68620ebdd0341d4cf828f8c2ae8e9f
Mahdi Yasini <yasinimahdi98@gmail.com>
2021-02-10 17:27:14 +0330

    ensureAdminAuthenticated

--- config/auth.js
+++ config/auth.js
@@ -5,5 +5,14 @@ module.exports = {
         }
         req.flash('error_msg', 'لطفا وارد شوید');
         res.redirect('/login');
+    },
+    ensureAdminAuthenticated: function(req, res, next) {
+        if(req.isAuthenticated() && req.user.userName === "Administrator") {
+            return next();
+        }
+        req.flash('error_msg', 'لطفا وارد شوید');
+        res.redirect('/login');
     }
+
 };
+

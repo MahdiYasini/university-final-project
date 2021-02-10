cfacd30c79780d606c828ca7bccf392cf2281a3d
Mahdi Yasini <yasinimahdi98@gmail.com>
2021-02-10 17:29:50 +0330

    add suspend property

--- Models/User.js
+++ Models/User.js
@@ -25,6 +25,10 @@ const UserSchema = new mongoose.Schema({
         type : Date, 
         default: Date.now
     },
+    suspend: {
+        type: Boolean,
+        default: false
+    }
 },
 {
     timestamps: true
